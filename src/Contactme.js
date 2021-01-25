ec1bcf8fdcd697e8e111743ed3b631d110551c2f
DongMZ1 <915094594@qq.com>
2021-01-24 20:09:23 -0500

    update contact form

--- src/Contactme.js
+++ src/Contactme.js
@@ -4,12 +4,26 @@ import { Translation } from 'react-i18next';
 import './CSS/homepage.css'
 import './CSS/contactme.css'
 import {AiFillWechat} from 'react-icons/ai'
+import emailjs from 'emailjs-com';
 class Contactme extends Component {
     constructor(props) {
         super(props);
         this.state = {  }
+        this.sendEmail = this.sendEmail.bind(this);
     }
 
+    sendEmail(e) {
+        e.preventDefault();
+    
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      }
+    
+    
     
     render() { 
         return ( 
@@ -22,6 +36,16 @@ class Contactme extends Component {
                            <div className="text-shadow-pop-top-2-1">
                        <AiFillWechat fontSize="40px"/> : Partington
                        </div>
+                       
+                       <form className="" onSubmit={this.sendEmail}>
+                         <label>Your Name</label>
+                         <input type="text" name="user_name" />
+                         <label>Email</label>
+                         <input type="email" name="user_email" />
+                         <label>Message</label>
+                         <textarea name="message" />
+                     <input type="submit" value="Submit" />
+    </form>
                        </div>
                        
                        <div className = "footer">{t('footer')}</div>
